a3aeb1c7ccf3980965ce7de93049aa1bac08d6d2
Ryan Stout <ryanstout@gmail.com>
2024-04-10 17:26:38 -0600

    - Move stoutlang.rb into lib, cleaner for path loads - Split visitor into a codegen stage and a generate stage - work on struct and property generation

--- lib/stoutlang.rb
+++ lib/stoutlang.rb
@@ -4,6 +4,15 @@ unless $LOAD_PATH.include?(lib_path)
   $LOAD_PATH << lib_path
 end
 
+# Add the macos llvm brew directory to the ffi search path
+require 'ffi'
+
+# Add the correct search path for llvm (on mac from brew)
+FFI::DynamicLibrary::SEARCH_PATH.unshift(`brew --prefix llvm@17`.strip + "/lib")
+
+require 'llvm'
+require 'llvm/core'
+
 require 'parser/parser'
 
 module StoutLang
