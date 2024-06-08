09e27a4b217ae4ba194a4f24690b1aca4bbd457b
Ryan Stout <ryanstout@gmail.com>
2024-06-08 14:50:47 -0600

    Rework import to do the heavy lifting in prepare. Make sure we clean up

--- lib/codegen/constructs/import.rb
+++ lib/codegen/constructs/import.rb
@@ -14,60 +14,37 @@ module StoutLang
       [Arg.new('path', TypeSig.new(type_val=Type.new('Str')))]
     end
 
-    # Import types and functions from a compile bitcode module. Compile it first
-    # if it's not already compiled
-    def codegen(compile_jit, mod, func, bb, import_call)
+    def prepare(import_call)
       # Read the path from the first argument
-      path = import_call.args[0].run
+      @path = import_call.args[0].run
 
-      cache_path = "builds/cache/#{path}"
-      FileUtils.mkdir_p("builds/cache/#{File.dirname(path)}")
+      cache_path = "builds/cache/#{@path}"
+      FileUtils.mkdir_p("builds/cache/#{File.dirname(@path)}")
 
       # Hash the file to see if it's changed
-      hash = hash_file_xxhash(path + ".sl") # TODO: this could be based off of the AST or something for more beefy speeds
+      hash = hash_file_xxhash(@path + ".sl") # TODO: this could be based off of the AST or something for more beefy speeds
 
       cache_path += "_#{hash}"
 
       if !File.exist?(cache_path+".bc")
         # Remove any previously cached files for this path
-        Dir.glob("builds/cache/#{path}_*").each do |file|
+        Dir.glob("builds/cache/#{@path}_*").each do |file|
           File.delete(file)
         end
 
         # Compile the file
-        Compiler.compile(path + ".sl", cache_path, {lib: true, aot: true})
+        Compiler.compile(@path + ".sl", cache_path, {lib: true, aot: true})
       end
 
-      original_module = LLVM::Module.parse_bitcode(cache_path + ".bc")
+      @original_module = LLVM::Module.parse_bitcode(cache_path + ".bc")
 
+      @imported_functions = []
       # Iterate over each function in the original module and create externs
-      original_module.functions.each do |function|
-        # TODO: The function pointers get overwritten when iterating (I think), so for now we don't use this
-        # looked up function
-        #
-        # Don't re-import
-        # TODO: janky
-        next if mod.functions.named(function.name)
-
+      @original_module.functions.each do |function|
         func_name, args, return_type = self.unmangle(function.name)
 
-
-        function = original_module.functions.named(function.name)
-
-        # Don't import internal functions from the library we're importing
-        # next if function.linkage == :private
-
-        # Create a prototype for the function
-        extern_function = mod.functions.add(
-          function.name.dup,
-          function.params.map(&:type).dup, # Map params to their types
-          function.type.return_type.dup,
-          # function.type.vararg? # Preserve var_arg status
-        )
         # extern_function.linkage = :private
         # extern_function = nil
-
-
 
         # Add the function to the scope
         if args
@@ -84,24 +61,61 @@ module StoutLang
           prototype = DefPrototype.new(func_name, args, return_type)
           prototype.parent = self
           prototype.prepare
-          prototype.ir = extern_function
         else
-          prototype = CPrototype.new(func_name, extern_function, nil)
+          prototype = CPrototype.new(func_name, nil)
         end
+        @imported_functions << [function, func_name, args, return_type, prototype]
+
+
         import_call.register_in_scope(func_name, prototype)
+      end
+    end
+
+    # Import types and functions from a compile bitcode module. Compile it first
+    # if it's not already compiled
+    def codegen(compile_jit, mod, func, bb, import_call)
+
+      @imported_functions.each do |function, func_name, args, return_type, prototype|
+        function = @original_module.functions.named(function.name)
+
+        # Don't import internal functions from the library we're importing
+        # next if function.linkage == :private
+        #
+
+        # TODO: The function pointers get overwritten when iterating (I think), so for now we don't use this
+        # looked up function
+        #
+        # Don't re-import
+        # TODO: janky
+        next if mod.functions.named(function.name)
+
+        # Create a prototype for the function
+        extern_function = mod.functions.add(
+          function.name.dup,
+          function.params.map(&:type).dup, # Map params to their types
+          function.type.return_type.dup,
+          # function.type.vararg? # Preserve var_arg status
+        )
+
+        prototype.ir = extern_function
       end
 
 
       # Don't link if we've already imported
+      #  TODO: Expose imports somehow inside of stoutlang
       @@imports ||= {}
-      if @@imports[path]
+      if @@imports[@path]
         return
       else
-        @@imports[path] = true
+        @@imports[@path] = true
 
         # Link the module into the root module
-        original_module.link_into(mod)
+        @original_module.link_into(mod)
       end
+
+      # Cleanup
+      @original_module = nil
+      @imported_functions = nil
 
     end
   end
